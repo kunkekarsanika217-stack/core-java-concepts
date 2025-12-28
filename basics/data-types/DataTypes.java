ce75ad8719b44833370c10827d4d7792f475e61a
Sanika Kunkekar <kunkekarsanika217@gmail.com>
2025-12-27 17:31:32 -0800

    Add java data types example

---
+++ basics/data-types/DataTypes.java
@@ -0,0 +1,39 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Primitive data types
+        byte byteValue = 10;
+        short shortValue = 100;
+        int intValue = 1000;
+        long longValue = 100000L;
+
+        float floatValue = 10.5f;
+        double doubleValue = 99.99;
+
+        char grade = 'A';
+        boolean isActive = true;
+
+        // Non-primitive data types
+        String name = "Java Developer";
+
+        int[] scores = {80, 85, 90};
+
+        Integer age = 22; // Wrapper class
+
+        // Printing values
+        System.out.println(byteValue);
+        System.out.println(shortValue);
+        System.out.println(intValue);
+        System.out.println(longValue);
+        System.out.println(floatValue);
+        System.out.println(doubleValue);
+        System.out.println(grade);
+        System.out.println(isActive);
+
+        System.out.println(name);
+        System.out.println(scores[0]);
+        System.out.println(age);
+ 
+    }
+}
