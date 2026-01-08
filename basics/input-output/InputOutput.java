f96ee0dba7816161784d0cc7f7eebb926b3859f8
Sanika Kunkekar <kunkekarsanika217@gmail.com>
2026-01-08 07:22:55 -0800

    Add input and output using scanner

---
+++ basics/input-output/InputOutput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class InputOutput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("\n--- User Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        scanner.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
